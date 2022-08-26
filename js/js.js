3ce37c8935ac5cc355c212d62362a77c26304849
Raja Oad <103353840+Rajabegginer@users.noreply.github.com>
2022-08-26 23:17:26 +0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,39 @@
+function menu() {
+    document.getElementById('navbar').style.display="block";
+    
+    document.getElementById('menus').style.display="none";
+    document.getElementById('cros').style.display="inline";
+    
+}
+
+function cross() {
+    document.getElementById('navbar').style.display="none";
+   
+    document.getElementById('menus').style.display="inline";
+    document.getElementById('cros').style.display="none";
+}
+
+function tableon() {
+    document.getElementById("tabon").style.display="block";
+    document.getElementById('taboff').style.display="inline";
+    document.getElementById('tabonn').style.display="none";
+}
+function tableoff() {
+    document.getElementById("tabon").style.display="none";
+    document.getElementById("tabonn").style.display="inline";
+    document.getElementById("taboff").style.display="none";
+}
+
+function pic() {
+    document.getElementById('pic').src="pic/raja2.jpg";
+    document.getElementById('text').innerHTML="My Name Is Rajkumar Oad";
+    document.getElementById('my-img').style.display="none";
+    document.getElementById('eren-img').style.display="inline";
+
+}
+function pic2() {
+    document.getElementById('pic').src="pic/eren.jpg";
+    document.getElementById('text').innerHTML="My Name Is Eren Jeager";
+    document.getElementById('my-img').style.display="inline";
+    document.getElementById('eren-img').style.display="none";
+}
